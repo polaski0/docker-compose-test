5e86236be615b6ccb68737de39653c116b7a722e
meynard3nidad <meynard.trinidad@diversifiedinfra.com>
2023-08-20 01:33:09 +0800

    Added unoptimized fetch requests

--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response, json } from "express";
 import config from "./config/config.json";
 import { getRoutes } from "./src/routes";
+import cors from 'cors';
 
 const app = express();
 
 app.use(json());
+app.use(cors());
 
 app.use('/api', getRoutes());
 
